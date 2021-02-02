5105b2214cf0bd96c14c838c9cee562a6b8d61bf
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-02 01:33:14 +0000

    working functions

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,9 +5,3 @@ const assertEqual = function (actual, expected) {
     console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, -1);
