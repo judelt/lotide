351fc4a175db549d250c1795441667a4fdeaa677
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-03 03:34:21 +0000

    fixed issues

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = function(array1, array2) {
+ 
+  for (let i = 0; i < array2.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
