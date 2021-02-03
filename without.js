59a7d2d6921f027c98117ce2752d6dcb213054d4
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-03 05:09:03 +0000

    without function

---
+++ without.js
@@ -0,0 +1,27 @@
+const eqArrays = function(array1, array2) {
+ 
+  for (let i = 0; i < array2.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const without = function (source, itemsToRemove) {
+  const newArray = [];
+  for (const element of source) {
+    if(!itemsToRemove.includes(element)) {
+      newArray.push(element);
+    }
+  }
+  console.log(newArray);
+};
