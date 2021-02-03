a720d9096140b736a4bd265c72d664e5e0db1d8e
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-03 21:39:19 +0000

    function to count the letters given a string

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const results = {};
+  for (const letter of string) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
