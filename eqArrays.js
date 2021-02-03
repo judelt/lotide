24d1272aea81b8e75dcd66cf7374760e45352ec4
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-03 03:37:22 +0000

    updated function

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,10 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+const eqArrays = function(array1, array2) {
+ 
+  for (let i = 0; i < array2.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
   }
+  return true;
 };
 
-const eqArrays = function (array1, array2) {
-  let truthy = false;
-  for (let i = 0; i < array2.length; i++) {
-    truthy = array1[i] === array2[i] ? true : false;   
-  }
-  return truthy;
-}
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
