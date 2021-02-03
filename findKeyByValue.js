4f0190e8d4e7de15177af0e1867ff0a523788e4e
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-03 23:21:40 +0000

    findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,9 @@
+const findKeyByValue = function (object, value) {
+  let result;
+  for(const key in object) {
+    if(object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+}
