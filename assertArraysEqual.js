e805d069e29cb6c73ff1f97eeceac37544806ed7
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-08 21:51:08 +0000

    refactored function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,9 +2,9 @@ const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+    console.log(`✅✅✅ Assertion Passed`);
   } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
+    console.log(`🚫🚫🚫 Assertion Failed`);
   }
 };
 
--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = function(array1, array2) {
-//  console.log(array1);
-//  console.log(array2);
-  for (let i = 0; i < array2.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   const arrayLength = array.length;
   const newArray = [];
@@ -39,10 +18,4 @@ const middle = function(array) {
   return newArray;
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
