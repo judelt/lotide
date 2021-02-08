f643b4f6a4b0a78d3f440b8092d7b7bc73a5ef5d
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-08 21:42:22 +0000

    refactored function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,4 @@
-const eqArrays = function(array1, array2) {
- 
-  for (let i = 0; i < array2.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
