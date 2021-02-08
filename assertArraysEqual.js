129ee21b7841bb4a77f842e69fef9ae4912d9a30
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-08 21:38:58 +0000

    refactored function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -15,3 +15,5 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
+
+module.exports = assertArraysEqual;
--- tail.js
+++ tail.js
@@ -1,3 +1,5 @@
 const tail = function (array) {
   return array.slice(1);
 };
+
+module.exports = tail;
